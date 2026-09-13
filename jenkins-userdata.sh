58551bd8c4f58b14ca6e63571d74af6f689a455e
George Akwari <devops4george@gmail.com>
2026-09-13 15:48:42 -0500

    updated jenkins userdata

--- jenkins-userdata.sh
+++ jenkins-userdata.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 sudo dnf update -y
-
-sudo dnf install -y wget java-21-amazon-corretto-devel git
-
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://jenkins.io
-sudo rpm --import https://jenkins.io
-
+sudo dnf install wget chkconfig shadow-utils fontconfig -y
+sudo dnf install java-21-amazon-corretto-devel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key
 sudo dnf clean all
-sudo dnf install -y jenkins
-
+sudo dnf install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
